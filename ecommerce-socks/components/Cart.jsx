427f783db2c08dd694be5c4455f1c6069d82af41
Oleh Subotin <subotinoleg0@gmail.com>
2022-10-18 18:20:57 +0300

    cart

--- ecommerce-socks/components/Cart.jsx
+++ ecommerce-socks/components/Cart.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import Link from 'next/link';
+import React, { useRef } from 'react';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from "react-icons/ti";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
+
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+
   return (
-    <div>Cart</div>
-  )
-}
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button
+          type="button"
+          className='cart-heading'
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className='heading'>Твій кошик</span>
+          <span className='cart-num-items'>({totalQuantities} штук)</span>
+        </button>
+        {
+          cartItems.length < 1 && (
+            <div className='empty-cart'>
+              <AiOutlineShopping size={200} />
+              <h3>Твій кошик пустий.</h3>
+              <Link href="/">
+                <button
+                  type="button"
+                  className='btn'
+                  onClick={() => setShowCart(false)}
+                >
+                  Продовжити закупівлю
+                </button>
+              </Link>
+            </div>
+          )
+        }
+
+        <div className='product-container'>
+          {
+            cartItems.length >= 1 && cartItems.map((item) => (
+              <div className='product' key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className='cart-product-image'
+                />
+                <div className='item-desc'>
+                  <div className='flex top'>
+                    <h5>{item?.name}</h5>
+                    <h5>{item?.price} гр.</h5>
+                  </div>
+                  <div className='flex bottom'>
+                    <div>
+                      <p className='quantity-desc'>
+                        <span className='minus' onClick=''><AiOutlineMinus /></span>
+                        <span className='num' onClick="">0</span>
+                        <span className='plus' onClick=''><AiOutlinePlus /></span>
+                      </p>
+                    </div>
+                    <button
+                      type='button'
+                      className='remove-item'
+                      onClick=""
+                    >
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+        {
+          cartItems.length >= 1 && (
+            <div className='cart-bottom'>
+              <div className='total'>
+                <h3>Підсумок:</h3>
+                <h3>{totalPrice} гр.</h3>
+              </div>
+              <div className='btn-container'>
+                <button
+                  type='button'
+                  className='btn'
+                  onClick=""
+                >
+                  Сплатити
+                </button>
+              </div>
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+};
 
 export default Cart
