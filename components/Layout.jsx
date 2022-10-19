da64e87177dfcf18e15754b2249e33d2f2266fd2
Oleh Subotin <subotinoleg0@gmail.com>
2022-10-19 12:27:43 +0300

    title

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
     <div className='layout'>
       <Head>
         <title>
-          Бабушка в'яже
+          Бабушка вяже
         </title>
       </Head>
       <header>
